5448d126c4f04a7b8179db2665607c72d912cd6c
carl.wang <carl.wang@augwit.com>
2017-10-25 18:27:56 +0800

    Init demo

---
+++ src/main/java/com/augwit/carl/demo/utils/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.augwit.carl.demo.utils;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Carl
+ * @Description:
+ * @Date: Created in 3:32 PM 10/25/2017
+ * @Modified By:
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+}
