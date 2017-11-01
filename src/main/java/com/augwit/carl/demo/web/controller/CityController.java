480f5437c4cbf9d2575c9ec4470b99081ac73c2f
carl.wang <carl.wang@augwit.com>
2017-11-01 10:14:04 +0800

    achieve excel upload and download

--- src/main/java/com/augwit/carl/demo/web/controller/CityController.java
+++ src/main/java/com/augwit/carl/demo/web/controller/CityController.java
@@ -2,27 +2,17 @@ package com.augwit.carl.demo.web.controller;
 
 import com.augwit.carl.demo.domain.City;
 import com.augwit.carl.demo.persistence.CityRepository;
-import com.augwit.carl.demo.utils.GeneratorXls;
 import com.augwit.carl.demo.utils.UploadExcel;
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFDateUtil;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.*;
 
 /**
@@ -46,42 +36,41 @@ public class CityController {
         int rowNum=sheet.getPhysicalNumberOfRows();
         int cellNum=sheet.getRow(0).getLastCellNum();
 
-        //1.把标题值获得 2.把值对应为一个有顺序的数组 3.city中装入对应好顺序的数组。
-        //建立一个有序的数组
-        ArrayList<String> orderedTitle=new ArrayList<>();
-        orderedTitle.add(0,"城市id");
-        orderedTitle.add(1,"城市名称");
-        orderedTitle.add(2,"城市面积");
-        orderedTitle.add(3,"所属省份");
-        orderedTitle.add(4,"邮编");
-        ArrayList<String> titles=new ArrayList<>();
-        for(int z=0;z<cellNum;z++){
-            String title= (String) uploadExcel.getCellValue(0,z);
-            titles.add(title);
-        }
-        for(int j=0;j<orderedTitle.size();j++){
-            int n=titles.indexOf(orderedTitle.get(j));
-            if(n!=j){
-                Collections.swap(orderedTitle,j,n);
-            }
-        }
+        //创建一个HashMap来存放Excel标题的字段
+        Map<String,Integer> titles=new HashMap();
 
-        //通过Excel标题获得所在列号，然后把列号赋给Map中对应于数据库表的名称的Value，Map(名称,对应列号)
-        //在city获得值时根据Map的列号获得值。
-
+        //获得Excel中第一行的标题字段
+        for (int j=0;j<cellNum;j++){
+            Object cellValue=uploadExcel.getCellValue(0,j);
+            titles.put((String) cellValue,j);
+        }
+        //将数据塞入表中
         for(int i=1;i<rowNum;i++) {
             City city=new City();
-            List<Object> list=new ArrayList<>();
-            for(int j=0;j<cellNum;j++){
-                list.add(uploadExcel.getCellValue(i,j));
-            }
-            city.setCityId(((Double) list.get(orderedTitle.indexOf("城市id"))).longValue());
-            city.setCityName((String) list.get(orderedTitle.indexOf("城市名称")));
-            city.setCityArea((Double) list.get(orderedTitle.indexOf("城市面积")));
-            city.setProvince((String) list.get(orderedTitle.indexOf("所属省份")));
-            city.setPostalCode((String) list.get(orderedTitle.indexOf("邮编")));
+
+            Integer colCityId=titles.get("城市id");
+            Object valueCityId=uploadExcel.getCellValue(i,colCityId);
+            Double doubleCityId= (Double) valueCityId;
+            city.setCityId(doubleCityId.longValue());
+
+            Integer colCityName=titles.get("城市名称");
+            Object valueCityName=uploadExcel.getCellValue(i,colCityName);
+            city.setCityName((String) valueCityName);
+
+            Integer colCityArea=titles.get("城市面积");
+            Object valueCityArea=uploadExcel.getCellValue(i,colCityArea);
+            city.setCityArea((Double) valueCityArea);
+
+            Integer colProvince=titles.get("所属省份");
+            Object valueProvince=uploadExcel.getCellValue(i,colProvince);
+            city.setProvince((String) valueProvince);
+
+            Integer colPostalCode=titles.get("邮编");
+            Object valuePostalCode=uploadExcel.getCellValue(i,colPostalCode);
+            city.setPostalCode((String) valuePostalCode);
+
             cityRepository.save(city);
-            }
+        }
         return "Success Uploaded !";
     }
 
